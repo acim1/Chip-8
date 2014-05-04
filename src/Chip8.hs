05908c3f3d5e83f14d6301c2ea23d9484fda43bf
acim1 <mkeller02@gmail.com>
2014-05-04 12:26:46 -0500

    Refactored getter/setter style, added display, waiting methods.

--- src/Chip8.hs
+++ src/Chip8.hs
@@ -32,82 +32,84 @@ type Keyboard  = [Word8]
 type Display   = [[Word8]] -- 64 (w) x 32 (l) pixels
 
 data Chip8 = C8 {
-    regs     :: Registers,
-    i        :: I,
-    dt       :: DT,
-    st       :: ST,
-    pc       :: PC,
-    stack    :: Stack,
-    ram      :: RAM,
-    randG    :: StdGen,
-    keyboard :: Keyboard,
-    display  :: Display
+    regs      :: Registers,
+    i'        :: I,
+    dt'       :: DT,
+    st'       :: ST,
+    pc'       :: PC,
+    stack     :: Stack,
+    ram       :: RAM,
+    randg'    :: StdGen,
+    keyboard' :: Keyboard,
+    display'  :: Display,
+    waiting'  :: Bool -- waiting for input
 } deriving Show
 
 -- Construction
 mkChip8 :: StdGen -> Chip8
 mkChip8 g = loadData c8 0x000 hexSprites  
   where c8 = C8 {
-    regs     = M.empty, 
-    i        = 0x000, 
-    dt       = 0x00, 
-    st       = 0x00, 
-    pc       = 0x200, -- typical program starting address
-    stack    = [], 
-    ram      = M.empty,
-    randG    = g, -- for opcodes which require randomness
-    keyboard = [], -- currently depressed keys
-    display  = replicate 32 $ replicate 8 0x00 -- 32 rows of 8-length lists of 8-bit units of pixels (64 bits)
+    regs      = M.empty, 
+    i'        = 0x000, 
+    dt'       = 0x00, 
+    st'       = 0x00, 
+    pc'       = 0x200, -- typical program starting address
+    stack     = [], 
+    ram       = M.empty,
+    randg'    = g, -- for opcodes which require randomness
+    keyboard' = [], -- currently depressed keys
+    display'  = replicate 32 $ replicate 8 0x00, -- 32 rows of 8-length lists of 8-bit units of pixels (64 bits)
+    waiting'  = False
   }
 
 -- Load program/program data in contiguous addresses
 loadData :: Chip8 -> Address -> [Word8] -> Chip8
 loadData c8 k [] = c8
-loadData c8 k (x:xs) = loadData (setMem c8 k x) (k+1) xs
+loadData c8 k (x:xs) = loadData (memSet c8 k x) (k+1) xs
 
 -- Registers
-setReg :: Chip8 -> RegN -> Word8 -> Chip8
-setReg _  k _ | k < 0x0 || k > 0xF = error "Can only update registers 0-F"
-setReg c8 k x = c8 {regs = M.insert (fromIntegral k) x (regs c8)} 
+regSet :: Chip8 -> RegN -> Word8 -> Chip8
+regSet _  k _ | k < 0x0 || k > 0xF = error "Can only update registers 0-F"
+regSet c8 k x = c8 {regs = M.insert (fromIntegral k) x (regs c8)} 
 
-getReg :: Chip8 -> RegN -> Word8
-getReg _  k | k < 0x0 || k > 0xF = error "Can only retrieve registers 0-F"
-getReg c8 k = M.findWithDefault 0x00 (fromIntegral k) $ regs c8
+reg :: Chip8 -> RegN -> Word8
+reg _  k | k < 0x0 || k > 0xF = error "Can only retrieve registers 0-F"
+reg c8 k = M.findWithDefault 0x00 (fromIntegral k) $ regs c8
 
 
 -- Special Registers
-getI :: Chip8 -> I
-getI = i
+iSet :: Chip8 -> Address -> Chip8
+iSet c8 k = c8 {i' = k}
 
-setI :: Chip8 -> Address -> Chip8
-setI c8 k = c8 {i = k}
+i :: Chip8 -> I
+i = i'
 
-getPC :: Chip8 -> PC
-getPC = pc
+pcSet :: Chip8 -> Address -> Chip8
+pcSet c8 k = c8 {pc' = k}
 
-setPC :: Chip8 -> Address -> Chip8
-setPC c8 k = c8 {pc = k}
+pc :: Chip8 -> PC
+pc = pc'
 
-setDT :: Chip8 -> Word8 -> Chip8
-setDT c8 x = c8 {dt = x}
+dtSet :: Chip8 -> Word8 -> Chip8
+dtSet c8 x = c8 {dt' = x}
 
-getDT :: Chip8 -> Word8
-getDT = dt
+dt :: Chip8 -> Word8
+dt = dt'
 
-setST :: Chip8 -> Word8 -> Chip8
-setST c8 x = c8 {st = x}
+stSet :: Chip8 -> Word8 -> Chip8
+stSet c8 x = c8 {st' = x}
 
-getST :: Chip8 -> Word8
-getST = st 
+st :: Chip8 -> Word8
+st = st' 
 
 -- RAM
-setMem :: Chip8 -> Address -> Word8 -> Chip8
-setMem _  n _ | n < 0x000 || n > 0xFFF = error "Can only update addresses 0x000-0xFFF"
-setMem c8 n x = c8 {ram = M.insert (fromIntegral n) x (ram c8)}
+memSet :: Chip8 -> Address -> Word8 -> Chip8
+memSet _  n _ | n < 0x000 || n > 0xFFF = error "Can only update addresses 0x000-0xFFF"
+memSet c8 n x = c8 {ram = M.insert (fromIntegral n) x (ram c8)}
 
-getMem :: Chip8 -> Address -> Word8
-getMem _  n | n < 0x000 || n > 0xFFF = error "Can only retrieve addresses 0x000-0xFFF"
-getMem c8 n = M.findWithDefault 0x000 (fromIntegral n) $ ram c8
+mem :: Chip8 -> Address -> Word8
+mem _  n | n < 0x000 || n > 0xFFF = error "Can only retrieve addresses 0x000-0xFFF"
+mem c8 n = M.findWithDefault 0x000 (fromIntegral n) $ ram c8
 
 -- Stack
 peak :: Chip8 -> Address
@@ -129,21 +131,32 @@ push c8 n = c8 {stack = push' (stack c8) n}
     push' xs x = x:xs
 
 -- Random Generator
-setRandG :: Chip8 -> StdGen -> Chip8
-setRandG c8 g = c8 {randG = g}
+randgSet :: Chip8 -> StdGen -> Chip8
+randgSet c8 g = c8 {randg' = g}
 
-getRandG :: Chip8 -> StdGen
-getRandG = randG
+randg :: Chip8 -> StdGen
+randg = randg'
 
 -- Keyboard
+keyboardSet :: Chip8 -> Keyboard -> Chip8
+keyboardSet c8 xs = c8 {keyboard' = xs}
 
-setKeyboard :: Chip8 -> Keyboard -> Chip8
-setKeyboard c8 xs = c8 {keyboard = xs}
+keyboard :: Chip8 -> Keyboard
+keyboard = keyboard'
 
-getKeyboard :: Chip8 -> Keyboard
-getKeyboard = keyboard
+-- Display
+displaySet :: Chip8 -> Display -> Chip8
+displaySet c8 xs = c8 {display' = xs}
 
--- Display...TODO
+display :: Chip8 -> Display
+display = display'
+
+-- Waiting for input
+waitingSet :: Chip8 -> Bool -> Chip8
+waitingSet c8 x = c8 {waiting' = x}
+
+waiting :: Chip8 -> Bool
+waiting = waiting'
 
 -- Pre-loaded Hex Digit Sprites
 hexSprites :: [Word8]
