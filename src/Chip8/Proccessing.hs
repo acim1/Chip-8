343510090b8efb62c23836b09dd66c289fa49be0
acim1 <mkeller02@gmail.com>
2014-05-09 19:46:59 -0500

    Refactored to "Processing" module instead of "Process"

--- src/Chip8/Proccessing.hs
+++ src/Chip8/Proccessing.hs
@@ -1,4 +1,4 @@
-module Chip8.Process
+module Chip8.Processing
 (
 ) where
 
@@ -11,9 +11,5 @@ import Data.IntMap.Lazy as M
 import System.Random
 
 -- Executes one Chip8 instruction cycle
-cycle :: Chip8 -> Chip8
-cycle = undefined
-
--- Execute a single instruction
-execute :: Chip8 -> OpCode -> Chip8
-execute = undefined  
+nextCycle :: Chip8 -> Chip8
+nextCycle = undefined
