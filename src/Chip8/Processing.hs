2f1139df08406343c1e09f3d5b7f064e1d05cced
acim1 <mkeller02@gmail.com>
2014-05-09 21:04:59 -0500

    Began initial code for fetch, decode, execute cycle.

--- src/Chip8/Processing.hs
+++ src/Chip8/Processing.hs
@@ -3,6 +3,7 @@ module Chip8.Processing
 ) where
 
 import Chip8
+import Data.Bits
 import Data.List
 import Data.Maybe
 import Control.Monad.State.Lazy
@@ -12,4 +13,23 @@ import System.Random
 
 -- Executes one Chip8 instruction cycle
 nextCycle :: Chip8 -> Chip8
-nextCycle = undefined
+nextCycle c8 = exec . decode . fetch $ c8
+  where exec = execute c8 
+
+fetch :: Chip8 -> OpCode
+fetch c8 = let pc    = pcGet c8
+               byte1 = memGet c8 pc
+               byte2 = memGet c8 (pc+1)
+               opc   = shiftL  (fi byte1) 8
+               opc'  = opc .|. (fi byte2) 
+           in opc'    
+  where fi x = fromIntegral x :: Word16
+
+decode :: OpCode -> Op
+decode = undefined
+
+execute :: Chip8 -> Op -> Chip8
+execute c8 op = undefined  
+
+data Op = SYS -- 0nnn - SYS addr. Jump to a machine code routine at nnn.
+-- future instructions, Can expand defs to include nibble or word pieces, i.e. LD nibble
