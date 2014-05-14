f1b028926fdaff786ed1e54d59ab7169dfddcc2a
acim1 <mkeller02@gmail.com>
2014-05-13 20:55:27 -0500

    Refactored decode to use case statement, added more decodings up to 0x8xy1.

--- src/Chip8/Processing.hs
+++ src/Chip8/Processing.hs
@@ -25,36 +25,59 @@ fetch c8 = let pc    = pcGet c8
            in oc'    
 
 decode :: OpCode -> Op
-decode oc
-    | oc ==  0x00E0 = 
-        CLS
-    | oc ==  0x00EE = 
-        RET
-    | oc .&. 0xF000 == 0x1000 = 
-        JP (0x0FFF .&. oc)
-    | oc .&. 0xF000 == 0x2000 =
-        CALL (0x0FFF .&. oc)
-    | oc .&. 0xF000 == 0x3000 = 
-        SE (w8 $ shiftR (0x0F00 .&. oc) 8) (w8 $ 0x00FF .&. oc)
-    | oc .&. 0xF000 == 0x4000 =
-        SNE (w8 $ shiftR (0x0F00 .&. oc) 8) (w8 $ 0x00FF .&. oc)
-    | oc .&. 0xF000 == 0x5000 =
-        SE1 (w8 $ shiftR (0x0F00 .&. oc) 8) (w8 $ shiftR (0x00F0 .&. oc) 4)  
-    | oc .&. 0xF000 == 0x6000 =
-        LD  (w8 $ shiftR (0x0F00 .&. oc) 8) (w8 $ 0x00FF .&. oc)    
+decode oc = case hi4 of
+    0x000 ->
+        case oc of
+            0x00E0 -> 
+                CLS
+            0x00EE -> 
+                RET
+            _      ->
+                SYS nnn
+    0x1000 -> 
+        JP nnn
+    0x2000 ->
+        CALL nnn
+    0x3000 -> 
+        SE x kk
+    0x4000 ->
+        SNE x kk
+    0x5000 ->
+        SE1 x y 
+    0x6000 ->
+        LD  x kk 
+    0x7000 ->
+        ADD x kk
+    0x8000 ->
+        case lo4 of
+            0x0000 ->
+                LD1 x y
+            0x0001 ->
+                OR x y
+  where 
+    hi4 = oc .&. 0xF000
+    lo4 = oc .&. 0x000F
+    nnn = (0x0FFF .&. oc)
+    x   = (w8 $ shiftR (0x0F00 .&. oc) 8)
+    y   = (w8 $ shiftR (0x00F0 .&. oc) 4)
+    kk  = (w8 $ 0x00FF .&. oc)
 
 execute :: Chip8 -> Op -> Chip8
 execute c8 op = undefined  
 
-data Op =   
-    CLS               -- 00E0
+data Op =
+    SYS Address       -- 0nnn
+  | CLS               -- 00E0
   | RET               -- 00EE
   | JP Address        -- 1nnn
   | CALL Address      -- 2nnn
   | SE Vx Byte        -- 3xkk
   | SNE Vx Byte       -- 4xkk
   | SE1 Vx Vy         -- 5xy0
-  | LD Vx Byte        -- 6xkk - LD Vx, byte
+  | LD Vx Byte        -- 6xkk
+  | ADD Vx Byte       -- 7xkk
+  | LD1 Vx Vy         -- 8xy0
+  | OR Vx Vy          -- 8xy1
           deriving (Show)
 
 w8 :: (Integral a) => a -> Word8
