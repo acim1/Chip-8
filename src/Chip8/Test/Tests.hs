dd557ed905e83d87954a7480e8bc2a3f9f79a44b
acim1 <mkeller02@gmail.com>
2014-07-06 15:07:22 -0500

    Updated Tests module to use abbreviated names.

--- src/Chip8/Test/Tests.hs
+++ src/Chip8/Test/Tests.hs
@@ -9,18 +9,18 @@ import Debug.Trace
 import System.Random
 
 -- Test display simple sprite: Display "0" in position (0,0)
-test01 = do
+t1 = do
     arr <- mkPixelArray
     g   <- getStdGen
     let c8 = mkChip8 g
-    let (Draw (Just sprite)) = evalState helper01 c8
+    let (Draw (Just sprite)) = evalState t1h1 c8
     putStrLn $ show sprite
     writeSprite arr sprite
     showDisplay arr
 
-
-helper01 :: State Chip8 Display    
-helper01 =  do
+-- Test 1, Helper 1
+t1h1 :: State Chip8 Display    
+t1h1 =  do
     memUpdate 0x200 0xA0
     memUpdate 0x201 0x00
     memUpdate 0x202 0xD0
